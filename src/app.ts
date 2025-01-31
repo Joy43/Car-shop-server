9631488dcc93e36054221c4e4e14aee0a699394c
Joy43 <ssjoy43@gmail.com>
2025-01-31 19:12:03 +0600

    task complete korar start

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,31 @@
-import express, { Application,  Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
 const app: Application = express();
-// parsers api
+
+// Middleware
 app.use(express.json());
-app.use(cors());
-// -----api end point--------
-app.use('/api');
+app.use(cors()); // Uncomment this line if you need CORS support
 
+// Example API routes
+app.use('/api', (req: Request, res: Response) => {
+  res.send({ message: 'This is the /api endpoint' });
+});
 
-// -----root api endpoint------
+// Root API endpoint
 app.get('/', (req: Request, res: Response) => {
-    res.send({
-      status: true,
-      message: 'blog_post Server is running successfully 🏃🏽‍♂️➡️',
-    });
+  res.send({
+    status: true,
+    message: 'blog_post Server is running successfully 🏃🏽‍♂️➡️',
   });
-  
-  // Middleware to handle 404 errors
-  app.use("*", (req: Request, res: Response, ) => {
-    res.status(404).json({
-      status: false,
-      message: 'Resource not found'
-    });
+});
+
+// Middleware to handle 404 errors
+app.use('*', (req: Request, res: Response) => {
+  res.status(404).json({
+    status: false,
+    message: 'Resource not found',
   });
+});
 
 export default app;
---
+++ src/app/modeules/user/user.interface.ts
@@ -0,0 +1,14 @@
+import { USER_ROLE } from "./user.contant";
+
+export type TUser = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+  status: 'in-progress' | 'blocked';
+  isBlocked: boolean;
+  isDeleted: boolean;
+ 
+};
+export type TUserRole = keyof typeof USER_ROLE;
